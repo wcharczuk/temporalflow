6f279365a3f2b011857dcb8681a602566bf88e99
Will Charczuk <wcharczuk@users.noreply.github.com>
2025-05-17 13:49:21 -0700

    updates

--- generator_node.go
+++ generator_node.go
@@ -6,15 +6,11 @@ import (
 	"github.com/wcharczuk/go-incr"
 )
 
-type generatorNode[Inputs, Output any] struct {
-	n      *incr.Node
-	inputs []incr.Incr[Inputs]
+type generatorNode[Input, Output any] struct {
+	n     *incr.Node
+	input incr.Incr[Input]
 }
 
 func (gn *generatorNode[Inputs, Output]) Stabilize(ctx context.Context) error {
-	return nil
-}
-
-func (gn *generatorNode[Inputs, Output]) FinishStabilize(ctx context.Context) error {
 	return nil
 }
